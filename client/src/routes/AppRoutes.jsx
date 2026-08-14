ba5c4cc9fafbc594db2b28021faee81bc17b5ce9
avika mane <avika.mane.cc@gmail.com>
2026-08-14 14:12:36 +0530

    routes added

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,63 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import AdminDashboard from "../Pages/AdminDashboard";
+import FeatureTrainer from "../Pages/Admin/feature-trainer";
+
+function AppRoutes() {
+  return (
+    <Routes>
+
+      {/* =========================
+          ADMIN ROUTES
+          ========================= */}
+
+      {/* Admin Dashboard */}
+      <Route
+        path="/admin"
+        element={<AdminDashboard />}
+      />
+
+      {/* Trainer Management */}
+      <Route
+        path="/admin/trainers"
+        element={<FeatureTrainer />}
+      />
+      {/* Trainer */}
+        <Route
+          path="/trainer/dashboard"
+          element={
+            <Layout role="Trainer">
+              <TrainerDashboard />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/trainer/sessions"
+          element={
+            <Layout role="Trainer">
+              <TrainerSessions />
+            </Layout>
+          }
+        />
+        <Layout role="Trainee">
+        <Routes>
+          <Route path="/trainee/dashboard" element={<TraineeDashboard />} />
+        </Routes>
+      </Layout> 
+
+      {/* =========================
+          DEFAULT ROUTE
+          ========================= */}
+
+      <Route
+        path="*"
+        element={<Navigate to="/admin" replace />}
+      />
+
+
+    </Routes>
+  );
+}
+
+export default AppRoutes;
